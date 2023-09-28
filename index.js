d36a305f16f8ac168517d7c97320a2a2b77894b7
Yesubabu0826 <21pa1a0501@vishnu.edu.in>
2023-09-28 20:06:10 +0530

    UPl

---
+++ index.js
@@ -0,0 +1,133 @@
+var express = require('express')
+var app = express()
+var bodyParser=require("body-parser");
+var session = require("express-session");
+var passwordHash= require('password-hash');
+const {v4:uuidv4} = require('uuid')
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore} = require('firebase-admin/firestore');
+var serviceAccount = require("./keyfirst.json")
+initializeApp({
+    credential: cert(serviceAccount)
+})
+const db = getFirestore();
+const PORT = process.env.PORT||3000
+app.set("view engine","ejs")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
+app.get("/",(req,res)=>{
+    res.render()
+})
+//sign up routes
+app.get('/signup',(req,res)=>{
+    res.render('signup',{validate:false});
+})
+app.post('/signup',(req,res)=>{
+    const first_name = (req.body.firstname).trim();
+    const user_name = (req.body.Username).trim();
+    const email = (req.body.email).trim();
+    const password = (req.body.password).trim();
+    var hashedPassword = passwordHash.generate(req.query.password);
+    db.collection("sandy")
+            .where("email", "==",email)
+            .get()
+            .then((docs) => {
+                if (docs.size > 0) {
+                    res.render('signup',{registered:"already registered"});
+                }
+                else {
+                    db.collection("sandy")
+                        .where("userName","==",user_name)
+                        .get()
+                        .then((docs)=>{
+                            if(docs.size>0){
+                                res.render('signup',{useregistered:"username exists"});
+                            }
+                            else{
+                                db.collection("sandy").add({
+                                    "name": first_name,
+                                    "userName": user_name,
+                                    "email": email,
+                                    "password": password
+                                }).then(() => {
+                                    res.render("signin");
+                                });
+                            }
+                        })
+                }
+        });
+})
+//sign in routes
+app.get('/signin',(req,res)=>{
+    res.render("signin")
+})
+app.post('/signin',(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    db.collection("sandy")
+     .where("email","==",email)
+     .where("password","==",password)
+     .get()
+     .then((docs)=>{
+        let verified = false;
+      ab.forEach((doc) => {
+        verified = passwordHash.verify(req.query.password,doc.data().password);});
+        if(docs.size>0){
+            
+            docs.forEach(doc=>{
+                req.session.user =doc._fieldsProto.userName.stringValue;
+                res.redirect("/home")
+            })
+        }
+        else{
+            res.end("Invalid username")
+        }
+    })
+})
+app.get("/home",(req,res)=>{
+    if(req.session.user){
+        db.collection("sandy")
+        .where("userName","==",req.session.user)
+        .get()
+        .then((docs)=>{
+            docs.forEach(doc=>{
+                res.render("home",{user:req.session.user,welcomename:doc._fieldsProto.name.stringValue})  
+            })
+        })   
+    }
+    else{
+        res.render('home')
+    }
+});
+//logout route
+app.get('/logout',(req, res)=>{
+        req.session.destroy((function (err) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                res.render("signin", {logout:"logout Successful"});
+            }
+        }));
+})
+//profile
+app.get("/profile",(req,res)=>{
+    console.log(req)
+})
+//about
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+//contact form
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
+app.listen(PORT,()=>{
+    console.log("Hey I am working")
+})
